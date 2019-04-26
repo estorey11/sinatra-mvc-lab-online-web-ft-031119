de015f415e3ca34f49054eea75b9220aa681ccf6
Eric Storey <estorey@gmail.com>
2019-04-26 22:08:24 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,9 +2,13 @@ class PigLatinizer
   def piglatinize(word)
     letters=word.split('')
     chunk=[]
-    letters.each do |letter|
+    letters.each_with_index do |letter|
       
     end
+  end
+  
+  def first_vowel(letters)
+    letters.first
   end
   
   def consonant?(letter)
