ec8c259f7b04721fe870641662509f645acd3c91
Eric Storey <estorey@gmail.com>
2019-04-26 22:18:13 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,6 +2,12 @@ class PigLatinizer
   def piglatinize(word)
     letters=word.split('')
     first_vowel_location=first_vowel_location(letters)
+    
+    if first_vowel_location==0 
+      letters << "w"
+    else 
+      
+    end
   end
   
   def first_vowel_location(letters)
